f9e64472530dc45f335da0d70704f163bd134304
Leonardo orellana <orellanaleonardo782@gmail.com>
2025-12-13 15:26:47 -0300

    fix: cors validation error

--- server-config.js
+++ server-config.js
@@ -43,12 +43,9 @@ export const createServer = () => {
       const allowedOrigins = [
         'http://localhost:3000',
         'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:4200',
-        'http://localhost:8080',
         'http://127.0.0.1:5173',
-        'http://127.0.0.1:5174',
         'https://nuri-task-app.vercel.app',
+        'https://nuri-task-api.vercel.app',
       ];
 
       if (!origin) {
